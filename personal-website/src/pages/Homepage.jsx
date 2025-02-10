72f61011ea6e968f4d7db2e3cbe51891255de64d
elenaktnguyen <elenanguyen2005@gmail.com>
2025-02-10 14:26:34 -0800

    style: format homepage to match wireframe

--- personal-website/src/pages/Homepage.jsx
+++ personal-website/src/pages/Homepage.jsx
@@ -7,33 +7,31 @@ function Homepage() {
     return (
         <>
             <Nav/>
-            <div className = "homeContent">
-                <div className = "homeImage">
+            <div className = "home-content">
+                <div className = "home-image">
                     <h1>Elena Nguyen</h1>
                 </div>
             </div>
 
-            <div className = "slide1">
-                <div className = "content1">
-                    <h1 className="title1">Website Overview</h1>
+            <div className = "home-1">
+                <div className = "content-1">
+                    <h1 className="home-title">Website Overview</h1>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ex commodi incidunt expedita est obcaecati omnis, perspiciatis harum rem facilis hic similique velit eos pariatur impedit dolore natus, nihil cupiditate.</p>
                     <h2 className = "subtitle1">Subtitle text</h2>
                 </div>
             </div>
 
             <hr/>
-            <div className = "slide2">
-                <div className = "content2" >
-                    <h1 className="title2">Card Description</h1>
-                    
+            <div className = "home-2">
+                <h1 className="home-title">Card Description</h1>
+                <div className = "content-2" >
                     <p><a href="#"><img class="card" src={card} alt="Card to [page]"></img></a></p>
                     <p><a href="#"><img class="card" src={card} alt="Card to [page]"></img></a></p>
                     <p><a href="#"><img class="card" src={card} alt="Card to [page]"></img></a></p>
                     <p><a href="#"><img class="card" src={card} alt="Card to [page]"></img></a></p>
                     <p><a href="#"><img class="card" src={card} alt="Card to [page]"></img></a></p>       
-                
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ex commodi incidunt expedita est obcaecati omnis, perspiciatis harum rem facilis hic similique velit eos pariatur impedit dolore natus, nihil cupiditate.</p>
                 </div>
+                <p className = "card-desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, ex commodi incidunt expedita est obcaecati omnis, perspiciatis harum rem facilis hic similique velit eos pariatur impedit dolore natus, nihil cupiditate.</p>
             </div>
             <Footer/>
         </>
