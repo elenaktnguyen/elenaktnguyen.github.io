8b7557d05c0e5359fd72a57c00aac61bc37e84c0
elenaktnguyen <elenanguyen2005@gmail.com>
2025-02-10 15:44:56 -0800

    style: format illustration page elements to match wireframe
    gallery is a placeholder div so images can be added later. added functionality to 'take a look!' button as well

--- personal-website/src/pages/Illustration.jsx
+++ personal-website/src/pages/Illustration.jsx
@@ -9,12 +9,14 @@ function Illustration() {
                 <div className = "illustration-image">
                     <h1>Illustration page</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, quis officiis quidem sint corrupti necessitatibus assumenda. Beatae, ad consequatur!</p>
-                    <button type="button">Take a look!</button>
+                    <button type="button">
+                        <a href="#gallery">Take a look!</a>
+                    </button>
                 </div>
             </div>
 
             <div className = "illustration-slide-1" >
-                {/* <h1 className="curtains">Sample text</h1> */}
+                <h1 className="" id="gallery">My Gallery</h1>
                 <div className = "illustration-content-1">
                     {/* Placeholder, upload art as needed*/}
                 </div>
