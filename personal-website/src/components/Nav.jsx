e922c185fc75e8ce1dcdbe7a6cfcfb2d436c90b2
elenaktnguyen <elenanguyen2005@gmail.com>
2025-02-10 04:58:25 -0800

    feat: add image placeholder for logo

--- personal-website/src/components/Nav.jsx
+++ personal-website/src/components/Nav.jsx
@@ -1,14 +1,18 @@
+import logo from "../assets/temp-logo.png"; /*og dimensions: 768 x 768 */
+
 function Nav() {
     return (
         <>
-        <header className = 'nav-container'>
-            <h1 className = 'logo'><a href="#">Logo Placeholder</a></h1>
-            <div className = 'nav-content'>
-                <nav className = 'nav-left'>
+        <header className = 'nav-container'> 
+            <div className = 'nav-content'> 
+                <nav className = 'nav-left'> 
                     <h4><a href="#">Illustrations</a></h4>
                     <h4><a href="#">Projects</a></h4>
                 </nav>
-                <nav className = 'nav-right'>
+                <a href="#">
+                    <img class="logo" src={logo} alt="Logo Placeholder"></img>
+                </a>
+                <nav className = 'nav-right'> 
                     <h4><a href="#">About</a></h4>
                     <h4><a href="#">LinkedIn</a></h4>
                 </nav>
