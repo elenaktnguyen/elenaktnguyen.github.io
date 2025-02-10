5474f393a736a7a22bd02c7c21fbb73d11676be8
elenaktnguyen <elenanguyen2005@gmail.com>
2025-02-10 03:53:06 -0800

    feat: add menu names and logo placeholder to nav

---
+++ personal-website/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+function Nav() {
+    return (
+        <>
+        <header>
+            <h1>Logo Placeholder</h1>
+            <div>
+                <h4><a href="#">Illustrations</a></h4>
+                <h4><a href="#">Projects</a></h4>
+                <h4><a href="#">About</a></h4>
+                <h4><a href="#">LinkedIn</a></h4>
+            </div>
+        </header>
+        </>
+    );
+}
+
+export default Nav;
