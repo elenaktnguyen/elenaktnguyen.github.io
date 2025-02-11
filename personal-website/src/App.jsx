e61bd691dea6057ce326dfd41ae3cc1c64a33592
elenaktnguyen <elenanguyen2005@gmail.com>
2025-02-10 23:38:40 -0800

    feat: add content to about page
    includes elements from #6

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,11 +1,12 @@
 import Homepage from './pages/Homepage.jsx'
 import Illustration from './pages/Illustration.jsx'
 import Projects from './pages/Projects.jsx'
+import About from './pages/About.jsx'
 
 function App() {
     return(
      <>
-      <Projects/>
+      <About/>
      </>
     )
 }
